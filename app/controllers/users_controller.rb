c1c4988634a1843ea37f44ad7304084e8ffaa331
bryanmoon1991 <bryanmoon1991@gmail.com>
2020-10-20 18:14:50 -0400

    user can edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    before_action :find_user, only: [:show, :edit]
+    before_action :find_user, only: [:show, :edit, :update]
     
     def index
         @users = User.all
@@ -13,12 +13,12 @@ class UsersController < ApplicationController
     end
 
     def create 
-        @user = User.new(user_params)
+        @user = User.new(user_params(:first_name, :last_name, :username, :password, :age, :email, :address, :phone_number))
 
         if @user.save
             redirect_to user_path(@user)
         else
-            flash[:error] = @user.errors.full_messages
+            flash[:errors] = @user.errors.full_messages
             redirect_to new_user_path
         end
     end
@@ -27,11 +27,11 @@ class UsersController < ApplicationController
     end
 
     def update
-        if @user.update(user_params)
+        if @user.update(user_params(:first_name, :last_name, :username, :age, :email, :address, :phone_number))
             redirect_to user_path(@user)
         else
-            flash[:error] = @user.errors.full_messages
-            redirect_to edit_user_path
+            flash[:errors] = @user.errors.full_messages
+            redirect_to edit_user_path(@user)
         end
     end
 
@@ -41,8 +41,8 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
     end
 
-    def user_params
-        params.require(:user).permit(:first_name, :last_name, :username, :password, :age, :email, :address, :phone_number)
+    def user_params(*args)
+        params.require(:user).permit(*args)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
     has_many :jobs, through: :saved_jobs
     has_secure_password
     
-    validates :username, uniqueness: true
+    validates :username, uniqueness: true, presence: true
     validates :email, uniqueness: true
     validates :age, presence: true
     validates :address, presence: true
-    validates :phone_number, presence: true
+    validates :phone_number, presence: true, numericality: true
     
     def full_name
         self.first_name + " " + self.last_name
