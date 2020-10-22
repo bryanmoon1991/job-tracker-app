d9b147fe01b7a9b379b9656bc9026781925983cd
bfeldman <brian.feldman42@gmail.com>
2020-10-22 13:57:51 -0400

    # of other savers feature

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,5 +6,9 @@ Dotenv.load('./.env')
 class Job < ApplicationRecord
     has_many :saved_jobs
     has_many :users, through: :saved_jobs
+    
+    def total_savers
+        SavedJob.all.find_all{|sj| sj.job_id == self.id }.count
+    end
 
 end
--- app/models/saved_job.rb
+++ app/models/saved_job.rb
@@ -3,4 +3,5 @@ class SavedJob < ApplicationRecord
   belongs_to :job
   
   validates :job_id, uniqueness: { scope: :user_id }
+
 end
